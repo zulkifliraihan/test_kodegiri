94f2348f1b57454c8091a878e2fb01d7c1f14c95
Zulkifli Raihan <zuran2907@gmail.com>
2023-07-05 00:22:18 +0700

    Delete Queue Process in Queue Config

--- config/queue.ts
+++ config/queue.ts
@@ -1,6 +1,4 @@
 import Queue from 'bull';
-import axios from 'axios';
-import prisma from './prisma';
 
 const queue = new Queue('birthdayQueue', {
   redis: {
@@ -15,41 +13,6 @@ const queue = new Queue('birthdayQueue', {
     },
   },
 });
-
-// queue.process('send-email', async (job) => {
-//   const { email, message } = job.data;
-
-//   try {
-//     const response = await axios.post('https://email-service.digitalenvision.com.au/send-email', { email, message });
-
-//     if (response.status == 200) {
-//       const user = await prisma.user.findUnique({ where: { email } })
-
-//       if (user) {
-//         user
-//         const updatedUser = await prisma.user.update({
-//           where: { id: user.id },
-//           data: {
-//             profiles: {
-//               update: {
-//                 lastReminderBirthdayAt: new Date()
-//               }
-//             },
-//           },
-//         });
-//       }
-//     }
-    
-//   } catch (error: any) {
-//     if (job.attemptsMade < 3) {
-//       console.log('Scheduling retry for job');
-//       throw error; 
-//     } else {
-//       console.log('Max retries reached for job');
-//     }
-//     console.error('API error:', error.message);
-//   }
-// });
 
 queue.on('completed', (job) => {
   console.log(`Job ${job.id} has been completed.`);
